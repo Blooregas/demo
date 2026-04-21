88c0e9fbb77f5dca22aba73a2c755ff5f496ec3f
Blooregas <onlain.zadrot@gmail.com>
2026-04-21 14:43:54 +0300

    Delete demo_beauty/app directory

--- demo_beauty/app/states/admin.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class AddServiceFSM(StatesGroup):
-    waiting_for_name = State()       # Название услуги (строка)
-    waiting_for_price = State()      # Цена (число)
-    waiting_for_duration = State()   # Длительность в минутах (число)
--- demo_beauty/app/states/llm.py
+++
@@ -1,4 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class AIChatFSM(StatesGroup):
-    chatting = State() # Клиент находится в режиме диалога с ИИ
